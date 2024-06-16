8e9432609b111d52965c6b0d36ba547228f3599c
fabianHCDCS270 <2023622802@student.uitm.edu.my>
2024-06-16 08:47:28 +0800

    - general update

---
+++ app/src/main/java/com/example/mtsfaz/forPage_recentDonation.java
@@ -0,0 +1,35 @@
+package com.example.mtsfaz;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class forPage_recentDonation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recentdonation_pending);
+
+        List<String> months = Arrays.asList(
+                "January", "February", "March", "April",
+                "May", "June", "July", "August", "September",
+                "October", "November", "December");
+
+        Spinner spinner = findViewById(R.id.dropdown_recentDonation);
+
+        ArrayAdapter adapter = new ArrayAdapter<>(
+                getApplicationContext(),
+                R.layout.spinner_txtview_selected_item,
+                months);
+
+        adapter.setDropDownViewResource(R.layout.spinner_txtview_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+}
