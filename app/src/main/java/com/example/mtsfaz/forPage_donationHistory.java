ddf4104a62cef8c155f0cab983dc77d1621f07fa
fabianHCDCS270 <2023622802@student.uitm.edu.my>
2024-06-17 00:04:14 +0800

    - activity_donationhistory.xml page siap

---
+++ app/src/main/java/com/example/mtsfaz/forPage_donationHistory.java
@@ -0,0 +1,35 @@
+package com.example.mtsfaz;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class forPage_donationHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donationhistory);
+
+        List<String> months = Arrays.asList(
+                "January", "February", "March", "April",
+                "May", "June", "July", "August", "September",
+                "October", "November", "December");
+
+        Spinner spinner = findViewById(R.id.dropdown_donationHistory);
+
+        ArrayAdapter adapter = new ArrayAdapter<>(
+                getApplicationContext(),
+                R.layout.spinner_txtview_selected_item,
+                months);
+
+        adapter.setDropDownViewResource(R.layout.spinner_txtview_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+}
